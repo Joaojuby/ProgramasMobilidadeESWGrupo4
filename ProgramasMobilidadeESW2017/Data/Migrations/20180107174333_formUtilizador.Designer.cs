154d837fbdfc77da1a8a202761bc21cb94120bd6
João Ricardo Gonçalves da Silva <joaojuby@live.com>
2018-01-07 17:51:55 +0000

    Adicionada migração para novos dados de utilizador

--- ProgramasMobilidadeESW2017/Data/Migrations/20180107174333_formUtilizador.Designer.cs
+++ ProgramasMobilidadeESW2017/Data/Migrations/20180107174333_formUtilizador.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace ProgramasMobilidadeESW2017.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180107174333_formUtilizador")]
+    partial class formUtilizador
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
