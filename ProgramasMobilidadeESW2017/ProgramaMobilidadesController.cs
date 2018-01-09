840aaba728fd80ff8aaa4264c6ad0d6a82e587af
João Ricardo Gonçalves da Silva <joaojuby@live.com>
2018-01-09 22:39:59 +0000

    Adicionadas views e controller para Programa Mobilidade

---
+++ ProgramasMobilidadeESW2017/ProgramaMobilidadesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProgramasMobilidadeESW2017.Data;
+using ProgramasMobilidadeESW2017.Models;
+
+namespace ProgramasMobilidadeESW2017
+{
+    public class ProgramaMobilidadesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProgramaMobilidadesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProgramaMobilidades
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.ProgramasMobilidade.Include(p => p.TipoProgramaMobilidade);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: ProgramaMobilidades/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var programaMobilidade = await _context.ProgramasMobilidade
+                .Include(p => p.TipoProgramaMobilidade)
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (programaMobilidade == null)
+            {
+                return NotFound();
+            }
+
+            return View(programaMobilidade);
+        }
+
+        // GET: ProgramaMobilidades/Create
+        public IActionResult Create()
+        {
+            ViewData["TipoProgramaMobilidadeID"] = new SelectList(_context.TiposProgramaMobilidade, "ID", "ID");
+            return View();
+        }
+
+        // POST: ProgramaMobilidades/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Nome,Descricao,TipoProgramaMobilidadeID,Duracao,DataInicioInscricao,DataLimiteInscricao")] ProgramaMobilidade programaMobilidade)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(programaMobilidade);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TipoProgramaMobilidadeID"] = new SelectList(_context.TiposProgramaMobilidade, "ID", "ID", programaMobilidade.TipoProgramaMobilidadeID);
+            return View(programaMobilidade);
+        }
+
+        // GET: ProgramaMobilidades/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var programaMobilidade = await _context.ProgramasMobilidade.SingleOrDefaultAsync(m => m.ID == id);
+            if (programaMobilidade == null)
+            {
+                return NotFound();
+            }
+            ViewData["TipoProgramaMobilidadeID"] = new SelectList(_context.TiposProgramaMobilidade, "ID", "ID", programaMobilidade.TipoProgramaMobilidadeID);
+            return View(programaMobilidade);
+        }
+
+        // POST: ProgramaMobilidades/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Nome,Descricao,TipoProgramaMobilidadeID,Duracao,DataInicioInscricao,DataLimiteInscricao")] ProgramaMobilidade programaMobilidade)
+        {
+            if (id != programaMobilidade.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(programaMobilidade);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProgramaMobilidadeExists(programaMobilidade.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TipoProgramaMobilidadeID"] = new SelectList(_context.TiposProgramaMobilidade, "ID", "ID", programaMobilidade.TipoProgramaMobilidadeID);
+            return View(programaMobilidade);
+        }
+
+        // GET: ProgramaMobilidades/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var programaMobilidade = await _context.ProgramasMobilidade
+                .Include(p => p.TipoProgramaMobilidade)
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (programaMobilidade == null)
+            {
+                return NotFound();
+            }
+
+            return View(programaMobilidade);
+        }
+
+        // POST: ProgramaMobilidades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var programaMobilidade = await _context.ProgramasMobilidade.SingleOrDefaultAsync(m => m.ID == id);
+            _context.ProgramasMobilidade.Remove(programaMobilidade);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProgramaMobilidadeExists(int id)
+        {
+            return _context.ProgramasMobilidade.Any(e => e.ID == id);
+        }
+    }
+}
