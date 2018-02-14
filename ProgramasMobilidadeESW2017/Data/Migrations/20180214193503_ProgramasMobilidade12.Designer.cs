7b806d12c5c3933358d0f32e94c212bc61b8ea43
Joaojuby <joaojuby@gmail.com>
2018-02-14 19:41:30 +0000

    Adiciona campos de imagens aos tipos de programas e paises

--- ProgramasMobilidadeESW2017/Data/Migrations/20180214193503_ProgramasMobilidade12.Designer.cs
+++ ProgramasMobilidadeESW2017/Data/Migrations/20180214193503_ProgramasMobilidade12.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace ProgramasMobilidadeESW2017.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180214193503_ProgramasMobilidade12")]
+    partial class ProgramasMobilidade12
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProgramasMobilidadeESW2017/Data/Migrations/20180214193503_ProgramasMobilidade12.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ProgramasMobilidadeESW2017.Data.Migrations
+{
+    public partial class ProgramasMobilidade12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "URLImagem",
+                table: "TiposProgramaMobilidade",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "URLBandeira",
+                table: "Paises",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "URLImagem",
+                table: "TiposProgramaMobilidade");
+
+            migrationBuilder.DropColumn(
+                name: "URLBandeira",
+                table: "Paises");
+        }
+    }
+}
